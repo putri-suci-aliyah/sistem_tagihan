0731b0616fd579d7a4a9060d4c400a2915a4837f
putri.suci.aliyah@gmail.com <putri.suci.aliyah@gmail.com>
2025-05-20 21:05:07 +0700

    Push pertama project besar

---
+++ sistem_tagihan_warga/database/migrations/2025_05_20_125340_create_wargas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('warga', function (Blueprint $table) {
+            $table->string('no_kk');
+            $table->unique('no_kk');
+            $table->string('nama');
+            $table->string('alamat');
+            $table->string('no_hp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('warga');
+    }
+};
---
+++ sistem_tagihan_warga/resources/views/pages/warga/index.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.main')
+
+@section('header')
+<div class="row mb-2">
+    <div class="col-sm-6">
+        <h1>Data Master Warga</h1>
+    </div>
+    <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active">Warga</li>
+        </ol>
+    </div>
+</div>
+@endsection
+
+@section('content')
+<div class="my-3 p-3 bg-body rounded shadow-sm">
+    <div class="pb-3">
+        <a href='{{ url('warga/create') }}' class="btn btn-primary">+ Tambah Data Warga</a>
+        <div class="d-inline-block ms-3">
+            <!-- Content inside the new div -->
+            <form class="d-flex" action="{{ url('warga') }}" method="get">
+                <input class="form-control me-1" type="search" name="katakunci" value="{{ Request::get('katakunci') }}" placeholder="Masukkan kata kunci" aria-label="Search">
+                <button class="btn btn-secondary" type="submit">Cari</button>
+            </form>
+        </div>
+    </div>
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th class="col-md-1">No</th>
+                <th class="col-md-2">No. Kartu Keluarga</th>
+                <th class="col-md-3">Nama</th>
+                <th class="col-md-3">Nomor HP</th>
+                <th class="col-md-4">Alamat Rumah</th>
+                <th class="col-md-2">Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php $no = $data->firstItem(); ?>
+            @foreach ($data as $item)
+            <tr>
+                <td>{{$no}}</td>
+                <td>{{$item->no_kk}}</td>
+                <td>{{$item->nama}}</td>
+                <td>{{$item->alamat}}</td>
+                <td>
+                    <!-- HREF KE HALAMAN EDIT DATA -->
+                    <a href='{{ url('warga/'.$item->no_kk.'/edit')}}' class="btn btn-warning btn-sm">Ubah</a>
+
+                    <!-- FORM HAPUS DATA -->
+                    <form onsubmit="return confirm('Apakah anda yakin ingin menghapus data ini?')" class="d-inline" action="{{ url('warga/'.$item->no_kk) }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger btn-sm">Hapus</button>
+                    </form>
+
+                </td>
+            </tr>
+            <?php $no++; ?>
+            @endforeach
+        </tbody>
+    </table>
+    {{ $data->withQueryString()->links() }}
+</div>
+@endsection
